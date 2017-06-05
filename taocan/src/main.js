fa3fbde58bff2ac01ad22713ccb5227a8354b39e
Eels <Cloted@hotmail.com>
2017-06-05 23:25:17 +0800

    语言包

--- taocan/src/main.js
+++ taocan/src/main.js
@@ -3,13 +3,28 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueI18n from 'vue-i18n'
 
-Vue.config.productionTip = false
+Vue.use(VueI18n)
+const i18n = new VueI18n({
+   locale: 'ja',
+   messages: {
+     ja: {
+       hello: 'こんにちは'
+     },
+     en: {
+       hello: 'hello'
+     }
+    }
+   })
+
+ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  i18n,
   template: '<App/>',
   components: { App }
 })
